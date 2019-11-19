a40fe85f35d21ccecba96b5b5a5a01529e320ad4
Riley W <r.wells6894@gmail.com>
2019-11-18 20:45:38 -0500

    Added Security Function
    Added getSha256Hash function for security utility.

--- PocketFinance/Utilities/Security.cs
+++ PocketFinance/Utilities/Security.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace PocketFinance.Utilities
 {
     class Security
     {
+
+        /// <summary>
+        /// Returns the hash of the parameter
+        /// 
+        /// TODO : Change this to SecureString
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string getHashSha256(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+
     }
 }
