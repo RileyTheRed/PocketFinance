8fd2cf5c62b209547c2e8bea685454d16e0e98e9
Riley W <r.wells6894@gmail.com>
2019-12-21 15:08:11 -0500

    New Income Page and View Model
    Added xaml for NewIncomePage and the corresponding view model. Just copied and modified the new expense page and view model

---
+++ PocketFinance/NewIncomePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using PocketFinance.Models;
+using PocketFinance.ViewModels;
+using Xamarin.Forms;
+
+namespace PocketFinance
+{
+    public partial class NewIncomePage : ContentPage
+    {
+
+        public ContentPage parentPage;
+        NewIncomePageViewModel viewModel;
+
+        public NewIncomePage(ContentPage parent, RecordBook book)
+        {
+            InitializeComponent();
+            parentPage = parent;
+            viewModel = new NewIncomePageViewModel(this, book);
+            BindingContext = viewModel;
+        }
+
+        void btnClicked_ReturnButton(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = parentPage;
+        }
+    }
+}
