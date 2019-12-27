3abfbb9ef618df4492f00cf28fd058f05951c3f1
Riley W <r.wells6894@gmail.com>
2019-12-27 15:24:22 -0500

    Updated SearchRecordPage files
    View model now updated for information to be passed to it. Properties created, properly bound now.
    
    XAML updated for custom viewcell template, appropriate data bound. 
    
    Same with the xaml.cs

--- PocketFinance/SearchRecordsPage.xaml.cs
+++ PocketFinance/SearchRecordsPage.xaml.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using PocketFinance.Models;
+using PocketFinance.ViewModels;
 using Xamarin.Forms;
 
 namespace PocketFinance
 {
     public partial class SearchRecordsPage : ContentPage
     {
-        public SearchRecordsPage()
+
+        MainPage parentPage;
+        //SearchRecordsPageViewModel viewModel;
+
+        public SearchRecordsPage(MainPage parent, RecordBook book)
         {
             InitializeComponent();
+            //viewModel = new SearchRecordsPageViewModel(this, book);
+            BindingContext = new SearchRecordsPageViewModel(this, book);
         }
     }
 }
--- PocketFinance/View Models/SearchRecordsPageViewModel.cs
+++ PocketFinance/View Models/SearchRecordsPageViewModel.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using PocketFinance.Models;
+
 namespace PocketFinance.ViewModels
 {
     public class SearchRecordsPageViewModel
     {
-        public SearchRecordsPageViewModel()
+
+        #region Properties
+        SearchRecordsPage parentPage;
+        public RecordBook recordBook
         {
+            get;
+            set;
+        }
+        #endregion
+
+        public SearchRecordsPageViewModel(SearchRecordsPage parent, RecordBook book)
+        {
+            parentPage = parent;
+            recordBook = book;
         }
     }
 }
