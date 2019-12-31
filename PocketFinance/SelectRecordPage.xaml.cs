f4a00f4d009c6e14bc24211b0b0124c73bcb3a8a
Riley W <r.wells6894@gmail.com>
2019-12-30 23:57:32 -0500

    SelectRecord page xaml and code behind created
    The xaml and code behind of the SelectRecord page is now created. None of the visual elements have been bound to its view model.
    
    its view model hasnt been created yet. but this will be the view when you click on a record that is displayed in the search view.

---
+++ PocketFinance/SelectRecordPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PocketFinance
+{
+    public partial class SelectRecordPage : ContentPage
+    {
+        public SelectRecordPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
