35da9c89bb5f2b22ae6bb3bd416309cc8075cc19
Riley W <r.wells6894@gmail.com>
2020-01-26 17:15:17 -0500

    Categorical Details and Total Monthly Income vs Total Monthly Expense
    View charts page now gives detailed information about the categorical expenses for the month, as well as their historical averages.
    
    At the bottom of the view charts page there is also information about the total monthly income vs total monthly expense, to give a better idea to the user at the end of the month what their contributions to savings should be

--- PocketFinance/Utilities/ChartCalculations.cs
+++ PocketFinance/Utilities/ChartCalculations.cs
@@ -32,54 +32,44 @@ namespace PocketFinance.Utilities
             "#7B68EE"
         };
 
+        static int GetMonth(string month)
+        {
+            switch (month)
+            {
+                case "January":
+                    return 1;
+                case "February":
+                    return 2;
+                case "March":
+                    return 3;
+                case "April":
+                    return 4;
+                case "May":
+                    return 5;
+                case "June":
+                    return 6;
+                case "July":
+                    return 7;
+                case "August":
+                    return 8;
+                case "September":
+                    return 9;
+                case "October":
+                    return 10;
+                case "November":
+                    return 11;
+                case "December":
+                    return 12;
+                default:
+                    return 1;
+            }
+        }
+
         public static Entry[] GetBarChart(string year, string month, List<Record> records)
         {
 
             int y = int.Parse(year);
-            int m;
-
-            switch (month)
-            {
-                case "January":
-                    m = 1;
-                    break;
-                case "February":
-                    m = 2;
-                    break;
-                case "March":
-                    m = 3;
-                    break;
-                case "April":
-                    m = 4;
-                    break;
-                case "May":
-                    m = 5;
-                    break;
-                case "June":
-                    m = 6;
-                    break;
-                case "July":
-                    m = 7;
-                    break;
-                case "August":
-                    m = 8;
-                    break;
-                case "September":
-                    m = 9;
-                    break;
-                case "October":
-                    m = 10;
-                    break;
-                case "November":
-                    m = 11;
-                    break;
-                case "December":
-                    m = 12;
-                    break;
-                default: // will never happen
-                    m = 1;
-                    break;
-            }
+            int m = GetMonth(month);
 
             // get a list of all the expense records for the year and month
             List<Record> tempList1 = records.Where(r =>
@@ -105,7 +95,6 @@ namespace PocketFinance.Utilities
                 }
                 total += item.Amount;
             }
-            //categoryTotals.Add("Total", total);
 
             // now create a list of entry values, and keep an inrement variable too
             int i = 0;
@@ -119,13 +108,110 @@ namespace PocketFinance.Utilities
                     ValueLabel = pair.Value.ToString()
                 }) ;
             }
-            //returns.Add(new Entry((float)total)
-            //{
-            //    Label = "Total"
-            //})
 
             return returns.ToArray();
 
+        }
+
+        public static List<CategoryReport> GetCategoryReports(string year, string month, List<Record> records)
+        {
+
+            //List<CategoryReport> returnList = new List<CategoryReport>();
+            Dictionary<string, CategoryReport> categoryReports = new Dictionary<string, CategoryReport>();
+            int y = int.Parse(year);
+            int m = GetMonth(month);
+
+            // get all expense records for this year and month
+            List<Record> tempCurrentYearAndMonth = records
+                .Where(r => r.Date.Year == y &&
+                            r.Date.Month == m &&
+                            r.RecordType.Equals("expense") &&
+                            !r.IsDeleted)
+                .ToList();
+
+            // start by getting each of the categories used this month
+            // also hold on to the total in order to add a TOTAL category at the end
+            double total = 0.0;
+            foreach (Record item in tempCurrentYearAndMonth)
+            {
+                if (!categoryReports.ContainsKey(item.Category))
+                {
+                    categoryReports.Add(item.Category, new CategoryReport(item.Category));
+                }
+                categoryReports[item.Category].Amount += item.Amount;
+                total += item.Amount;
+            }
+            // TODO in category code, make sure that user can not enter TOTAL as a category name
+            // TODO in category comparison, implement a ToLower() or ToUpper() check ***
+            categoryReports.Add("TOTAL", new CategoryReport("TOTAL"));
+            categoryReports["TOTAL"].Amount = total;
+
+            // now get all expense records where the records is NOT from this month AND year
+            List<Record> tempNotCurrentMonthOrYear = records
+                .Where(r => (r.Date.Year != y ||
+                            r.Date.Month != m) &&
+                            r.RecordType.Equals("expense") &&
+                            !r.IsDeleted)
+                .ToList();
+
+            // get the first month and year that record keeping began
+            DateTime firstYearAndMonth = DateTime.Now;
+            foreach (Record item in records)
+            {
+                if (!item.IsDeleted && (item.Date < firstYearAndMonth))
+                {
+                    firstYearAndMonth = item.Date;
+                }
+            }
+            int months = ((DateTime.Now.Year - firstYearAndMonth.Year) * 12)
+                + (DateTime.Now.Month - firstYearAndMonth.Month);
+
+            // for each of the categories in the category reports, get the total amount
+            // from previous months and calculated the average.
+            foreach (KeyValuePair<string, CategoryReport> pair in categoryReports)
+            {
+                double totalAmount = 0.0;
+
+                if (pair.Key.Equals("TOTAL"))
+                {
+                    totalAmount += tempNotCurrentMonthOrYear
+                    .Select(r => r.Amount)
+                    .Sum();
+                }
+                else
+                {
+                    totalAmount += tempNotCurrentMonthOrYear
+                        .Where(r => r.Category.Equals(pair.Key))
+                        .Select(r => r.Amount)
+                        .Sum();
+                }
+
+                if (months > 0)
+                {
+                    categoryReports[pair.Key].AverageAmount = totalAmount / months;
+                }
+                else
+                {
+                    categoryReports[pair.Key].AverageAmount = categoryReports[pair.Key].Amount;
+                }
+            }
+
+            return categoryReports.Values.ToList();
+
+        }
+
+        public static double GetTotalMonthlyIncome(string year, string month, List<Record> records)
+        {
+            int y = int.Parse(year);
+            int m = GetMonth(month);
+
+            return records
+                .Where(r => r.Date.Year == y &&
+                            r.Date.Month == m &&
+                            r.RecordType.Equals("income") &&
+                            !r.IsDeleted)
+                .Select(r => r.Amount)
+                .Sum();
         }
 
     }
